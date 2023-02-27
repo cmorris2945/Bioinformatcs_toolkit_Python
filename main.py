1f03de244cca13e6efa0c6a84ff6781322dc371d
cmorris2945 <cmorris2945@yahoo.com>
2023-02-26 22:55:11 -0500

    I renamed one of the modules

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 ## DNA tool set and where we test the code functions and files...
-from validate_count_nucleotide import *
+from DNA_Funct_Tools import *
+import random
 
-rand_DNA_String = "ATTGCCCGTAA"
+rand_DNA_String1 = "ATTGCCCGTAA"
 rand_DNA_String2 = "ATCcCGGX"
+random_DNA_String3 = ''.join([random.choice(Nucleotides)
+                              for nuc in range(20)])
 
-print(validateSeq(rand_DNA_String))
+
+
+print(validateSeq(rand_DNA_String1))
 
 print(validateSeq(rand_DNA_String2))
+
+print(validateSeq(random_DNA_String3))
